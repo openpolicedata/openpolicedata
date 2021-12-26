31463de6d819eaa12f07333c488514072283eedb
Paul Otto <potto@ieee.org>
2021-12-26 04:53:25 -0800

    Started support for  ArcGIS

--- src/OpenDataTable.py
+++ src/OpenDataTable.py
@@ -97,7 +97,7 @@ class OpenDataTable:
             pass
         elif self._dataType == datasets.DataTypes.GeoJSON:
             pass
-        elif self._dataType == datasets.DataTypes.REQUESTS:
+        elif self._dataType == datasets.DataTypes.ArcGIS:
             # TODO: Paul
             pass
         elif self._dataType == datasets.DataTypes.SOCRATA:
