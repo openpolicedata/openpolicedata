02614c101e8368822238d47674f20fa3cf1b1a85
Paul Otto <potto@ieee.org>
2022-01-12 15:14:26 -0800

    Added a test to check the ability of the code to download data from the URL.

--- tests/README.md
+++ tests/README.md
@@ -8,3 +8,15 @@ Next add the src directory to the Python path with
 
 Then run PyTest at the project root by typing
 > `pytest` 
+
+The above command will not run all the tests. To see which tests are skipped then run
+> `pytest -rs`
+
+Before running the tests which validate the ability of the code to download data, the gold validation data needs to be created. To do this first create a directory named data in the sandbox folder.
+> 'mkdir sandbox/data`
+
+Next run the Jupyter notebook `denver_generate_test_data.py` to generate the data files.
+
+Now run all the tests.
+
+> `pytest --runslow`
--- tests/test_arcgis.py
+++ tests/test_arcgis.py
@@ -1,4 +1,6 @@
 import pytest
+import pandas as pd
+import os
 import DataLoaders
 
 class TestProduct:
@@ -24,12 +26,16 @@ class TestProduct:
                 df=DataLoaders.loadArcGIS(url, dateField=dateField, year=year) 
 
 
-    @pytest.mark.skip(reason="This is a slow test and should only be run before a major commit.")
-    def test_url_arcgis_to_dataframe(self):
+    @pytest.mark.slow(reason="This is a slow test and should only be run before a major commit.")
+    def test_url_arcgis_to_dataframe(self):        
         url = 'https://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/ODC_CRIME_STOPS_P/FeatureServer'
         dateField = 'TIME_PHONEPICKUP'
-        year = 2021
-        df=DataLoaders.loadArcGIS(url, dateField=dateField, year=year)
-        print(df)
-        assert len(df)>0
+        gold_start_year = 2014
+        gold_end_year = 2016
+        gold_filename = f'./sandbox/data/police_pedestrian_stops_and_vehicle_stops.gold.{gold_start_year}_{gold_end_year}.csv'         
+        print(f'Comparing {url} with {gold_filename}')
+        print(f'Current directory is {os.getcwd()}')
+        df_gold = pd.read_csv(gold_filename)
+        df=DataLoaders.loadArcGIS(url, dateField=dateField, year=[gold_start_year, gold_end_year])
+        assert len(df) == len(df_gold)
         
