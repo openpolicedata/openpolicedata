f386ab4fada3fcadc59dbe802c3e07bb72acbcfd
sowdm <67804485+sowdm@users.noreply.github.com>
2022-01-29 14:09:49 -0500

    Update arcgis test with opd interface

--- tests/test_arcgis.py
+++ tests/test_arcgis.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 import sys
 from openpolicedata import data_loaders
+from openpolicedata import data
+from openpolicedata import datasets
 
 
 class TestProduct:
@@ -29,15 +31,22 @@ class TestProduct:
 
 
     @pytest.mark.slow(reason="This is a slow test and should only be run before a major commit.")
-    def test_url_arcgis_to_dataframe(self):        
-        url = 'https://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/ODC_CRIME_STOPS_P/FeatureServer'
-        date_field = 'TIME_PHONEPICKUP'
+    def test_url_arcgis_to_dataframe(self):
         gold_start_year = 2014
         gold_end_year = 2016
-        gold_filename = f'./data/police_pedestrian_stops_and_vehicle_stops.gold.{gold_start_year}_{gold_end_year}.csv'         
-        print(f'Comparing {url} with {gold_filename}')
-        print(f'Current directory is {os.getcwd()}')
-        df_gold = pd.read_csv(gold_filename)
-        df=data_loaders.loadArcGIS(url, date_field=date_field, year=[gold_start_year, gold_end_year])
-        assert len(df) == len(df_gold)
+        gold_filename = f'./data/police_pedestrian_stops_and_vehicle_stops.gold.{gold_start_year}_{gold_end_year}.csv'
+        if os.path.isfile(gold_filename):
+            print(f'Comparing {url} with {gold_filename}')
+            print(f'Current directory is {os.getcwd()}')
+            df_gold = pd.read_csv(gold_filename)
+        else;
+            csv_url = "https://www.denvergov.org/media/gis/DataCatalog/police_pedestrian_stops_and_vehicle_stops/csv/police_pedestrian_stops_and_vehicle_stops.csv"
+            df_gold = pd.read_csv(csv_url)
+            if not os.path.isdir(os.path.dirname(gold_filename)):
+                os.mkdir(os.path.dirname(gold_filename))
+            df_gold.to_csv(gold_filename)
+            
+        src = data.Source("Denver Police Department")
+        table = src.load_from_url([gold_start_year, gold_end_year], table_type=datasets.TableTypes.ST
+        assert len(table.table) == len(df_gold)
         
