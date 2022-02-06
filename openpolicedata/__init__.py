a64ac92c407aff9af4fcb96bda9973ed9721dee6
Paul Otto <potto@ieee.org>
2022-02-06 07:25:35 -0800

    modified __init__.py to have easy to access classes and data types

--- openpolicedata/__init__.py
+++ openpolicedata/__init__.py
@@ -1,15 +1,11 @@
 import sys
 import os
-print(f"The __init__.py system path is {sys.path} and current directory is {os.getcwd()}")
 
-#For all to work we need a path into /home/user/cjc/openpolicedata/openpolicedata
-__all__ = [ "data", "data_loaders", "datasets"]
+from .datasets import DataTypes
+from .datasets import TableTypes
+from .datasets import datasets
+from .datasets import get
+from .data import Source
 
 
 
-#These
-from openpolicedata.data_loaders import *
-from openpolicedata.datasets import *
-from openpolicedata.data import *
-#from openpolicedata import data_loaders
-#from openpolicedata import datasets
