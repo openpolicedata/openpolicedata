0b03ae7b597890d2c033c250aebf565b76fc9687
Paul Otto <potto@ieee.org>
2022-02-06 07:53:36 -0800

    Renamed module to not conflict with datasets variable. reflect new module name in init.py. this closes #26

--- openpolicedata/__init__.py
+++ openpolicedata/__init__.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-from .datasets import DataTypes
-from .datasets import TableTypes
-from .datasets import datasets
-from .datasets import get
+from ._datasets import DataTypes
+from ._datasets import TableTypes
+from ._datasets import datasets
+from ._datasets import get
 from .data import Source
 
 
