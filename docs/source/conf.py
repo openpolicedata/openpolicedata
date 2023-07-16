caf5a894c71410655d8264215986ff603ba02a54
Paul Otto <potto@ieee.org>
2023-07-16 14:32:11 -0700

    testing how to not have the build error: index.rst not found

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,8 +91,6 @@ html_theme_options = {
 
 source_suffix = {
     '.md': 'markdown',
-    '.rst': 'restructuredtext',
-    '.txt': 'restructuredtext',
 }
 
 # This removes execution counts when displaying Jupyter notebooks
