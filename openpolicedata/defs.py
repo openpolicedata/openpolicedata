eed117dd3d855adc16d23f66d02042e6ac098f95
sowdm <67804485+sowdm@users.noreply.github.com>
2023-10-28 20:20:14 -0400

    Fixing typo in defs.py

--- openpolicedata/defs.py
+++ openpolicedata/defs.py
@@ -83,7 +83,7 @@ class TableType(str, Enum):
         "Crash data may be split into several tables due to the possibility that multiple "+
         "subjects and vehicles may be involved in an incident. This table contains data on vehicles.")
     DISCIPLINARY_RECORDS = ("DISCIPLINARY RECORDS",
-        "Disciplinary reocrds of officers")
+        "Disciplinary records of officers")
     EMPLOYEE = ("EMPLOYEE","Demographic data of the police workforce")
     DEATHS_IN_CUSTODY = ("DEATHS IN CUSTODY", "Deaths that occur in custody or during the process of arrest")
     FIELD_CONTACTS = ("FIELD CONTACTS", "Consensual contacts between officers and the community.")
