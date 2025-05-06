beb3289efd1565a685ed4e529106ed92769d1fb3
Mickey P <me@minkedup.xyz>
2025-05-06 12:38:24 -0400

    Removed test_datasets_equals_civilian test fn
    Removed the test_datasets_equals_civilian because it tests replacement
    of types in TableType which is no longer relevant.

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -83,22 +83,6 @@ def test_datasets_no_civilian_tabletypes():
 	assert not df["TableType"].str.contains("CIVILIAN").any()
 
 
-def test_datasets_equals_civilian():
-	df = opd.datasets.query()
-	assert isinstance(df, DeprecationHandlerDataFrame)
-
-	s = df["TableType"]
-	assert isinstance(s, DeprecationHandlerSeries)
-	with pytest.deprecated_call():
-		t = s == "USE OF FORCE - CIVILIANS/OFFICERS"
-
-	assert t.sum()>0
-	s = df["TableType"][t]
-	assert isinstance(s, pd.Series)
-	assert len(s)>0
-	assert len(df[t])>0
-
-
 def test_datasets_equals_subjects():
 	df = opd.datasets.query()
 	assert isinstance(df, DeprecationHandlerDataFrame)
